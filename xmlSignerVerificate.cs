b8061db4c5a031e89c6e76df5e2248434f77743b
yasinciftcii <yasinciftci_32utlook.com>
2024-08-01 15:19:47 +0300

    xmlVerifier

---
+++ xmlSignerVerificate.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Runtime.Versioning;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+using System.Xml;
+
+[SupportedOSPlatform("Windows")]
+public class VerifyXML
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            // Create a new CspParameters object to specify
+            // a key container.
+            CspParameters cspParams = new()
+            {
+                KeyContainerName = "XML_DSIG_RSA_KEY"
+            };
+
+            // Create a new RSA signing key and save it in the container.
+            RSACryptoServiceProvider rsaKey = new(cspParams);
+
+            // Create a new XML document.
+            XmlDocument xmlDoc = new()
+            {
+                // Load an XML file into the XmlDocument object.
+                PreserveWhitespace = true
+            };
+            xmlDoc.Load("../signed_file.xml");
+
+            // Verify the signature of the signed XML.
+            Console.WriteLine("Verifying signature...");
+            bool result = VerifyXml(xmlDoc, rsaKey);
+
+            // Display the results of the signature verification to
+            // the console.
+            if (result)
+            {
+                Console.WriteLine("The XML signature is valid.");
+            }
+            else
+            {
+                Console.WriteLine("The XML signature is not valid.");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    // Verify the signature of an XML file against an asymmetric
+    // algorithm and return the result.
+    public static bool VerifyXml(XmlDocument xmlDoc, RSA key)
+    {
+        // Check arguments.
+        if (xmlDoc == null)
+            throw new ArgumentException(null, nameof(xmlDoc));
+        if (key == null)
+            throw new ArgumentException(null, nameof(key));
+
+        // Create a new SignedXml object and pass it
+        // the XML document class.
+        SignedXml signedXml = new(xmlDoc);
+
+        // Find the "Signature" node and create a new
+        // XmlNodeList object.
+        XmlNodeList nodeList = xmlDoc.GetElementsByTagName("Signature");
+
+        // Throw an exception if no signature was found.
+        if (nodeList.Count <= 0)
+        {
+            throw new CryptographicException("Verification failed: No Signature was found in the document.");
+        }
+
+        // This example only supports one signature for
+        // the entire XML document.  Throw an exception
+        // if more than one signature was found.
+        if (nodeList.Count >= 2)
+        {
+            throw new CryptographicException("Verification failed: More that one signature was found for the document.");
+        }
+
+        // Load the first <signature> node.
+        signedXml.LoadXml((XmlElement?)nodeList[0]);
+
+        // Check the signature and return the result.
+        return signedXml.CheckSignature(key);
+    }
+}
